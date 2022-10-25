15fb4c882942ebb3b5367535eedc3eba59d3901a
Doaa Albishri <duaa_ali@outlook.sa>
2022-10-25 11:10:28 +0300

    Completed Doctors page

---
+++ app/src/main/java/com/example/midcloud2/AllDoctorsActivity.kt
@@ -0,0 +1,11 @@
+package com.example.midcloud2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AllDoctorsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_all_doctors)
+    }
+}
